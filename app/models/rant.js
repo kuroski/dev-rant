3588728944582448b436e810638addf118f86906
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-02-29 21:01:04 +0100

    Add user avatar

--- app/models/rant.js
+++ app/models/rant.js
@@ -3,4 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class RantModel extends Model {
   @attr('string') text;
   @attr('string') author;
+  @attr('string') avatar;
 }
--- app/serializers/rant.js
+++ app/serializers/rant.js
@@ -4,6 +4,7 @@ export default class RantSerializer extends RESTSerializer {
   normalize(modelClass, resourceHash) {
     return super.normalize(modelClass, {
       ...resourceHash,
+      avatar: `https://avatars.devrant.com/${resourceHash.user_avatar.i}`,
       author: resourceHash.user_username
     })
   }
