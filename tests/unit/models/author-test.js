ec565e9631fb2c7e5e2344d709c3f4a2a2e5c4dc
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-03-15 16:18:51 +0100

    Test author and rant model relationships

--- tests/unit/models/author-test.js
+++ tests/unit/models/author-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { get } from '@ember/object';
 
 module('Unit | Model | author', function(hooks) {
   setupTest(hooks);
@@ -9,5 +10,18 @@ module('Unit | Model | author', function(hooks) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('author', {});
     assert.ok(model);
+  });
+
+  test('an author own rants', function(assert) {
+    const author = this.owner.lookup('service:store').modelFor('author');
+
+    const relationship = get(author, 'relationshipsByName').get('rants');
+
+    assert.equal(relationship.key, 'rants', 'has relationship with rant');
+    assert.equal(
+      relationship.kind,
+      'hasMany',
+      'kind of relationship is hasMany'
+    );
   });
 });
