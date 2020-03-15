d11f1bb4eadb1e63c550467a883350c9e6c706ad
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-03-15 16:18:31 +0100

    Improved rant serializer

--- app/serializers/rant.js
+++ app/serializers/rant.js
@@ -1,18 +1,34 @@
-import RESTSerializer, { EmbeddedRecordsMixin } from '@ember-data/serializer/rest';
+import RESTSerializer, {
+  EmbeddedRecordsMixin
+} from "@ember-data/serializer/rest";
 
-export default class RantSerializer extends RESTSerializer.extend(EmbeddedRecordsMixin) {
+export default class RantSerializer extends RESTSerializer.extend(
+  EmbeddedRecordsMixin
+) {
   attrs = {
-    author: { embedded: 'always' }
+    author: { embedded: "always" }
   };
+
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    return super.normalizeResponse(
+      store,
+      primaryModelClass,
+      { rants: payload.rants },
+      id,
+      requestType
+    )
+  }
 
   normalize(modelClass, resourceHash) {
     return super.normalize(modelClass, {
       ...resourceHash,
       author: {
-          id: resourceHash.user_id,
-          avatar: resourceHash.user_avatar.i && `https://avatars.devrant.com/${resourceHash.user_avatar.i}`,
-          username: resourceHash.user_username,
+        id: resourceHash.user_id,
+        avatar:
+          resourceHash.user_avatar.i &&
+          `https://avatars.devrant.com/${resourceHash.user_avatar.i}`,
+        username: resourceHash.user_username
       }
-    })
+    });
   }
 }
