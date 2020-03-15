5896c0b1a7004de022417de28d11eb65414b42cb
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-03-15 16:16:36 +0100

    Remove rant prefix only from application host url

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,9 +2,9 @@ import RESTAdapter from '@ember-data/adapter/rest';
 import TokenAuthorizerMixin from 'ember-simple-auth-token/mixins/token-authorizer';
 
 export default class ApplicationAdapter extends RESTAdapter.extend(TokenAuthorizerMixin) {
-  host = 'https://devrant.com/api/devrant';
+  host = 'https://devrant.com/api';
 
-  buildURL(modelName, id, snapshot, requestType, query) {
+  buildURL(modelName, id, snapshot, requestType, query = {}) {
     query['app'] = 3;
     return super.buildURL(modelName, id, snapshot, requestType, query);
   }
